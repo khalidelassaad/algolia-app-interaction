24be45ad53eb736efe7d02feef0ca6efb5a5ba54
Khalid Elassaad <khalidelassaad@gmail.com>
2022-07-11 18:23:31 -0700

    code: use case 1 completed (all files)

--- authtoalgolia.py
+++ authtoalgolia.py
@@ -1,9 +1,13 @@
 import json
 from algoliasearch.search_client import SearchClient
 
-def auth():
+def get_secrets_json():
     secrets_file = open("secrets.json", "r")
     secrets_json = json.load(secrets_file)
     secrets_file.close()
+    return secrets_json
+
+def auth():
+    secrets_json = get_secrets_json()
     client = SearchClient.create(secrets_json["appID"], secrets_json["writeApiKey"])
     return client
